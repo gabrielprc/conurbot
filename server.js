a5ebe523bc3d83c1873b9244d7c0deb1036c16bb
gabrielprc <gabrielpablorc@gmail.com>
2017-05-13 18:07:29 -0300

    Reddit API integration

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-var token = process.env.TELEGRAM_TOKEN;
+var http = require('http');
 var TelegramBot = require('node-telegram-bot-api');
-var bot = new TelegramBot(token, {polling: true});
 var Messenger = require('./messaging/messenger');
 var SubredditConsumer = require('./reddit/consumer');
+var port = process.env.PORT || 8080;
+var token = process.env.TELEGRAM_TOKEN;
+var bot = new TelegramBot(token, {polling: true});
 
-var messenger = new Messenger(bot);
-messenger.bind();
+var server = http.createServer(function(request, response) {
+	console.log(request.url)
+	response.end('Hello Node.js Server!')
+});
 
-var consumer = new SubredditConsumer('conurban', messenger);
-setInterval(consumer.get, 10000);
+server.listen(port, function(err){  
+	if (err) {
+	    return console.log('ufff, se rompió todo', err);
+	}
 
+	start()
+
+	console.log('conurbot has risen');
+});
+
+function start() {
+	var messenger = new Messenger(bot);
+	messenger.bind();
+
+	var consumer = new SubredditConsumer('conurban', messenger);
+	setInterval(consumer.get, 10000);
+}
